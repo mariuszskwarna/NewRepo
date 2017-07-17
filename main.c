7beff8d9d07035c0c70b361ee98b26d40bc2be09
mariusz <mariusz.skwarna@tieto.com>
2017-07-17 11:42:28 +0200

    org main

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include "fun.h"
+
 int main(int argc, char* argv[]){
+
+	int i=0;
+	scanf("Polskie menu:1 / English menu:2 : ",&i);
+	
+	if(i==1 || i==2)
+		menu(i);
+	else printf("Niepoprawna opcja / bad option");
+
 return 0
 }
